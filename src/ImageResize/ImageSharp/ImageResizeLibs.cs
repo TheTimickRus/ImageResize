bb82901c90ee2089ee8e62de00847e5a7ce781ef
TheTimickRus <andrey.timofeev.and37@gmail.com>
2022-05-21 13:13:49 +0300

    Добавлен проект

---
+++ src/ImageResize/ImageSharp/ImageResizeLibs.cs
@@ -0,0 +1,104 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageResize.ImageSharp;
+
+public static class ImageResizeLibs
+{
+    public static bool ExecuteForFile(FileInfo file, int resizePercentValue, int jpegQuality, out string newName)
+    {
+        try
+        {
+            var imgOutDir = file.DirectoryName;
+            var imgOutName = Path.GetFileNameWithoutExtension(file.Name);
+            var imgOutExt = Path.GetExtension(file.Name).ToLower();
+            var imgOutputFullName = Path.Combine(imgOutDir ?? "", $"{imgOutName}_Conv{imgOutExt}");
+            
+            using var img = Resize(file.FullName, resizePercentValue);
+
+            newName = Path.GetFileName(imgOutputFullName);
+            
+            switch (imgOutExt)
+            {
+                case ".jpg":
+                    img.SaveAsJpeg(imgOutputFullName, new JpegEncoder { Quality = jpegQuality });
+                    break; 
+                case ".png":
+                    img.SaveAsPng(imgOutputFullName);
+                    break;
+                default:
+                    return false;
+            }
+            
+            return true;
+        }
+        catch
+        {
+            newName = "";
+            return false;
+        }
+    }
+
+    public static bool ExecuteForFolder(string basePath, FileInfo file, int resizePercentValue, int jpegQuality)
+    {
+        try
+        {
+            using var img = Resize(file.FullName, resizePercentValue);
+            
+            var imgOutDir = file.DirectoryName?.Replace(basePath, $"{basePath}_Conv") ?? "";
+            var imgOutName = Path.GetFileNameWithoutExtension(file.Name);
+            var imgOutExt = Path.GetExtension(file.Name).ToLower();
+            var imgOutputFullName = Path.Combine(imgOutDir, $"{imgOutName}{imgOutExt}");
+
+            if (Directory.Exists(imgOutDir) is false)
+            {
+                Directory.CreateDirectory(imgOutDir);
+            }
+            
+            switch (imgOutExt)
+            {
+                case ".jpg":
+                    img.SaveAsJpeg(imgOutputFullName, new JpegEncoder { Quality = jpegQuality });
+                    break; 
+                case ".png":
+                    img.SaveAsPng(imgOutputFullName);
+                    break;
+                default:
+                    return false;
+            }
+            
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    /// <summary>
+    /// Уменьшение разрешения на Х процентов
+    /// </summary>
+    /// <param name="file">Путь до изображения</param>
+    /// <param name="resizePercentValue">Процент (X), на который нужно уменьшить разрешение изображения</param>
+    /// <returns>Изображение</returns>
+    private static Image? Resize(string file, int resizePercentValue)
+    {
+        try
+        {
+            var img = Image.Load(file);
+            
+            var percent = resizePercentValue / 100.0;
+            var newWidth = (int)(img.Width * percent);
+            var newHeight = (int)(img.Height * percent);
+            
+            img.Mutate(x => x.Resize(newWidth, newHeight));
+
+            return img;
+        }
+        catch
+        {
+            return null;
+        } 
+    }
+}
---
+++ src/ImageResize/Program.cs
@@ -0,0 +1,20 @@
+﻿using ImageResize;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+var app = new CommandApp<MainCommand>();
+app.Configure(configurator =>
+{
+    configurator.PropagateExceptions();
+});
+
+try
+{
+    return app.Run(args);
+}
+catch (Exception ex)
+{
+    AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+    Console.ReadKey();
+    return -1;
+}
